3cdc699aa745d23392780ca59c1f6a33db431ab7
tabuenam <juan.jose.mejia@hs-furtwangen.de>
2021-02-17 15:16:28 +0100

    Definition of class Widow v1 and namespaces for the game engine

--- Gamengine/Test.cpp
+++ Gamengine/Test.cpp
@@ -1,13 +1,24 @@
-#include<iostream>
-#include <GLFW/glfw3.h>
+#include"x64/Debug/src/graphics/window.h"
 
 int main()
 {
-	if (!glfwInit())
-		std::cout << "Error" << std::endl;
-	else
-		std::cout << "Success! \n";
+	gmJJ::graphics::Window window(800, 600, "GameEngineWindow");
+	/*Combination for a blue screen that is about to run in the while-loop*/
+	glClearColor(0.2f, 0.3f, 0.8f, 1.0f);
 
-	system("PAUSE");
+	std::cout << glGetString(GL_VERSION); 
+
+	while (!window.close())
+	{
+		std::cout << window.getHeight() << "," << window.getWidth() << "\n";
+		window.clear();
+		glBegin(GL_QUADS);
+		glVertex2f(-0.5f, -0.5f);
+		glVertex2f(-0.5f, 0.5f);
+		glVertex2f(0.5f, 0.5f);
+		glVertex2f(0.5f, -0.5f);
+		glEnd();
+		window.update();
+	}
 	return 0;
 }
