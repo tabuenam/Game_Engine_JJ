1a346e2d63f8cbafad2ba35eed080543bd2a2892
tabuenam <juan.jose.mejia@hs-furtwangen.de>
2021-02-17 16:40:54 +0100

    Glew Installation finished

--- Gamengine/Test.cpp
+++ Gamengine/Test.cpp
@@ -6,11 +6,13 @@ int main()
 	/*Combination for a blue screen that is about to run in the while-loop*/
 	glClearColor(0.2f, 0.3f, 0.8f, 1.0f);
 
-	std::cout << glGetString(GL_VERSION); 
+	GLuint var;
+	glGenVertexArrays(1, &var);
+	glBindVertexArray(var);
+
 
 	while (!window.close())
 	{
-		std::cout << window.getHeight() << "," << window.getWidth() << "\n";
 		window.clear();
 		glBegin(GL_QUADS);
 		glVertex2f(-0.5f, -0.5f);
@@ -18,6 +20,8 @@ int main()
 		glVertex2f(0.5f, 0.5f);
 		glVertex2f(0.5f, -0.5f);
 		glEnd();
+		glDrawArrays(GL_ARRAY_BUFFER, 0, 6);
+
 		window.update();
 	}
 	return 0;
