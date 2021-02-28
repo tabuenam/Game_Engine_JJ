16b7748fac8f458df5d54555905d1d76f27df406
tabuenam <juan.jose.mejia@hs-furtwangen.de>
2021-02-28 20:07:44 +0100

    CursorPosition and the inputs of the keyboard and mouse has been added on the class Window

--- Gamengine/Test.cpp
+++ Gamengine/Test.cpp
@@ -14,14 +14,19 @@ int main()
 	while (!window.close())
 	{
 		window.clear();
+		double x, y;
+		window.getMousePosition(x, y);
+		std::cout << x<<", " << y << std::endl;
+#if 1
 		glBegin(GL_QUADS);
 		glVertex2f(-0.5f, -0.5f);
 		glVertex2f(-0.5f, 0.5f);
 		glVertex2f(0.5f, 0.5f);
 		glVertex2f(0.5f, -0.5f);
 		glEnd();
+#else
 		glDrawArrays(GL_ARRAY_BUFFER, 0, 6);
-
+#endif
 		window.update();
 	}
 	return 0;
